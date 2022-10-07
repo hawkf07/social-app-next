053859327677b9c398c8fc208e2446f3f1eb9679
hawkf07 <hawkf07@proton.me>
2022-10-07 23:07:19 +0700

    feat: tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: { max: "768px" },
+      },
+    },
   },
   plugins: [],
 };
